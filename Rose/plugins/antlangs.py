78e7f0e77053df2710e889b187f58171c6e2b1b7
Imran95942 <halil-223@mail.ru>
2022-06-22 09:13:52 +0300

    Update antlangs.py

--- Rose/plugins/antlangs.py
+++ Rose/plugins/antlangs.py
@@ -276,20 +276,20 @@ async def check_anti_funcs(_, message: Message):
 __MODULE__ = f"{F_sub}"
 __HELP__ = """
 
-**ForceSubscribe | Channel manager:**
-- Rose can mute members who are not subscribed your channel until they subscribe
-- When enabled I will mute unsubscribed members and show them a unmute button. When they pressed the button I will unmute them.
+**ForceSubscribe | Channel manager:**.
+- Borz может отключать доступ к каналам не подписавшихся пользователей до тех пор, пока они не подпишутся.
+- Когда эта функция включена, я буду отключать звук у неподписанных пользователей и показывать им кнопку отключения звука. Когда они нажмут на кнопку, я отключу их.
 
-**Setup**
-1) First of all add me in the group as admin with ban users permission and in the channel as admin.
-Note: Only creator of the group can setup me and i will not allow force subscribe again if not done so.
+**Настройка**
+1) Прежде всего, добавьте меня в группу как администратора с правом запрета пользователей и в канал как администратора.
+Примечание: Только создатель группы может добавить меня, и я не позволю принудительно подписаться снова, если этого не сделать.
 
 
-**Commmands**
-- /forcesubscribe - To get the current settings.
-- /forcesubscribe no/off/disable - To turn of ForceSubscribe.
-- /forcesubscribe {channel username} - To turn on and setup the channel.
-- /forcesubscribe clear - To unmute all members who muted by me.
+**Команды**
+- /forcesubscribe - Получить текущие настройки.
+- /forcesubscribe no/off/disable - отключить принудительную подписку.
+- /forcesubscribe {имя пользователя канала} - Включить и настроить канал.
+- /forcesubscribe clear - отключить доступ всем пользователям, которые были отключены мной.
 
-Note: /forcesub is an alias of /forcesubscribe
+Примечание: /forcesub является псевдонимом /forcesubscribe
 """
